2f03e137f4783ffa2c5afacc8a42bc4fb5af02cf
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 04:36:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -20,7 +20,16 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.keys.each do
+    |season|
+    data[season].each do
+      |contestant|
+      # binding.pry
+      if contestant.values.include?(hometown) then count += 1 end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
